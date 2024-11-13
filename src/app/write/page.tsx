af57c461d05c0f5e0cb82fcc0bce0f8d8c2cc2f0
dagyeong Jeong <jdk09140@gmail.com>
2024-11-13 21:25:52 +0900

    front

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -4,11 +4,8 @@ export default function Write() {
   return (
     <div>
       <p>write</p>
-<<<<<<< HEAD
       <p>test</p>
       <p>test1</p>
-=======
->>>>>>> parent of 22a4e5e (test)
     </div>
   )
 }
