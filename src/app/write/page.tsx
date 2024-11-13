102288d4c13a4ca6ce6182bdf8ba9e94682a2b1a
dagyeong Jeong <jdk09140@gmail.com>
2024-11-13 21:20:58 +0900

    test11

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -4,8 +4,11 @@ export default function Write() {
   return (
     <div>
       <p>write</p>
+<<<<<<< HEAD
       <p>test</p>
       <p>test1</p>
+=======
+>>>>>>> parent of 22a4e5e (test)
     </div>
   )
 }
